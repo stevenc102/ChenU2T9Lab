e38dc4792408b4b775ed54920787ff088e639e37
1E10 Laptop <sample@bths.edu>
2022-10-14 12:05:35 -0400

    commit

---
+++ src/LabRunner.java
@@ -0,0 +1,8 @@
+public class LabRunner {
+    public static void main(String[] args) {
+        System.out.println(Math.floor(100.1));
+        System.out.println(Math.floor(999));
+        System.out.println(Math.toDegrees(2 * Math.PI));
+        System.out.println(Math.toRadians(180));
+    }
+}
